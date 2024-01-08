7bdde1e99b2be86be8a51b64b480c5e757b11d0e
majesnix <majesnix@electronicintelligence.de>
2024-01-08 23:58:26 +0100

    chore: add zap and sha256

--- Casks/aye-player-develop.rb
+++ Casks/aye-player-develop.rb
@@ -1,8 +1,7 @@
 cask "aye-player-develop" do
   version "0.9.2"
-  sha256 :no_check
+  sha256 "658151769b18717d3aec45e7da5cb9a941f6ec7889f157e646206a0ede283787"
 
-  #https://github.com/AYE-Player/aye-player/releases/download/v0.9.2/aye-player-0.9.2-arm64.dmg
   url "https://github.com/AYE-Player/aye-player/releases/download/v#{version}/aye-player-#{version}-arm64.dmg"
   name "Aye-Player"
   desc "A new Youtube Music Player to deliver you the best experience possible."
@@ -14,12 +13,9 @@ cask "aye-player-develop" do
 
   app "aye-player.app"
 
-  #zap trash: [
-  #  "~/Library/Application Support/io.playcover.PlayCover",
-  #  "~/Library/Caches/io.playcover.PlayCover",
-  #  "~/Library/Containers/io.playcover.PlayCover",
-  #  "~/Library/Frameworks/PlayTools.framework",
-  #  "~/Library/Preferences/io.playcover.PlayCover.plist",
-  #  "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
-  #]
+  zap trash: [
+    "~/Library/Application Support/aye-player",
+    "~/Library/Preferences/aye-player.plist",
+    "~/Library/Saved Application State/aye-player.savedState/",
+  ]
 end
